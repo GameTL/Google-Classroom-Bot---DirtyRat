a13c7bee4007b019c732ffac8ba179ddcb867754
GameTL <limsila.limsila@yahoo.com>
2021-05-04 18:28:55 +0700

    V2 - Logic & comment improvements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # To add a new markdown cell, type '# %% [markdown]'
 
 
-# %% [markdown]
+# %%
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,9 +11,8 @@ import time
 import re
 from bs4 import BeautifulSoup
 
-browser  = webdriver.Firefox()
+browser  = webdriver.Firefox() # this open a browser
 
-# %%
 class ClassRoom:
     def __init__(self, SimpleClassName, SimpleTeacherName, GCClass, GCTeacher, Link):
         self.SimpleClassName = SimpleClassName
@@ -21,6 +20,8 @@ class ClassRoom:
         self.GCClass = GCClass
         self.GCTeacher = GCTeacher
         self.Link = Link
+
+# %%
 
 list_of_classroom = []
 
@@ -33,35 +34,37 @@ print(list_of_classroom[0].SimpleClassName)
 # %%
 # The goal is to reach the homepage of the Homeroom
 def sign_in_function():
-    EMAIL = ""
-    PASSWORD = ""
+    EMAIL = "" ### ENTER USERNAME ###
+    PASSWORD = "" ### ENTER PASSWORD ###
     global browser
     browser.get("""https://accounts.google.com/signin/v2/identifier
     ?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=
-    GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin""")
+    GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin""") # Google Sign In Page
     browser.implicitly_wait(2)
     email_fill = browser.find_element_by_xpath("//input[contains(@type,'email')]").send_keys(EMAIL)
     email_click_next = browser.find_element_by_xpath("//div[contains(@class,'VfPpkd-RLmnJb')]").click()
     time.sleep(2)
     password_fill = browser.find_element_by_xpath("//input[contains(@class,'whsOnd zHQkBf')]").send_keys(PASSWORD)
     password_click_next = browser.find_element_by_xpath("//div[contains(@class,'VfPpkd-RLmnJb')]").click()
-    browser.get(Computer_Science.Link)
+    browser.get(Computer_Science.Link) ###  CHANGE VARIABLE HERE  ###
 
+sign_in_function()
 
+# %%
 def check_for_valid_post_time():
     global post_valid_time
-    time_list = ["3 May"]
+    time_list = ["3 May", "08:", "07:"]  ###  CHANGE VARIABLE HERE  ###
     OPERATOR = "x*"
     global post_valid_time
     post_valid_time = False
     time.sleep(4)
-    current_page = browser.page_source
-    full_html = BeautifulSoup(current_page, features="lxml")
-    content_html = full_html.get_text()
+    whole_page = browser.page_source
+    top_post = browser.find_element_by_xpath("""//div[contains(@class,'qhnNic LBlAUc Aopndd TIunU')]""").text
+    print(top_post)
     for time_string in time_list:
-        re_string = time_list[0] + OPERATOR
+        re_string = time_list[0] + OPERATOR # joining the search string with the operator
         print(re_string)
-        valid_time = re.findall(re_string, content_html)
+        valid_time = re.findall(re_string, top_post)
         print(valid_time)
         if valid_time:
             print("Yes, there is at least one match!")
@@ -70,7 +73,9 @@ def check_for_valid_post_time():
             print("No match")
             # PANIC!
 
+check_for_valid_post_time()
 
+# %%
 def fill_comment():
     if post_valid_time:
         time.sleep(5)
@@ -78,13 +83,13 @@ def fill_comment():
         comment_fill = browser.find_element_by_xpath("//div[contains(@class,'LsqTRb Lzdwhd-AyKMt tgNIJf-Wvd9Cc Yiql6e iTy5c editable')]").send_keys(FILL_LIST[2])
         time.sleep(4)
         click_reply_button = browser.find_element_by_xpath("//*[name()='path' and @d='M2 3v18l20-9L2 3zm2 11l9-2-9-2V6.09L17.13 12 4 17.91V14z']").click()
+        # for xpath for svg format is different for somereason
 
-sign_in_function()
-check_for_valid_post_time()
 fill_comment()
+
+
+
+# %% Quit 
 time.sleep(5)
 browser.quit()
 
-
-
-# %% 
